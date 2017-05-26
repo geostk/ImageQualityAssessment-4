e2eb0050acd2c01f7ac6f35176ecc20014609e3b
Scalman <waleedhassan@live.se>
2017-05-26 11:47:19 +0200

    dwecew

--- BRISQUE.py
+++ BRISQUE.py
@@ -1,3 +1,30 @@
+"""
+Copyright (c) 2011 The University of Texas at Austin
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without license or royalty fees, to use, copy, 
+modify, and distribute this code (the source files) and its documentation for
+any purpose, provided that the copyright notice in its entirety appear in all copies of this code, and the 
+original source of this code, Laboratory for Image and Video Engineering (LIVE, http://live.ece.utexas.edu)
+and Center for Perceptual Systems (CPS, http://www.cps.utexas.edu) at the University of Texas at Austin (UT Austin, 
+http://www.utexas.edu), is acknowledged in any publication that reports research using this code. The research
+is to be cited in the bibliography as:
+
+1) A. Mittal, A. K. Moorthy and A. C. Bovik, "BRISQUE Software Release", 
+URL: http://live.ece.utexas.edu/research/quality/BRISQUE_release.zip, 2011
+
+2) A. Mittal, A. K. Moorthy and A. C. Bovik, "No Reference Image Quality Assessment in the Spatial Domain"
+submitted
+
+IN NO EVENT SHALL THE UNIVERSITY OF TEXAS AT AUSTIN BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, 
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS DATABASE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF TEXAS
+AT AUSTIN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE UNIVERSITY OF TEXAS AT AUSTIN SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE DATABASE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+AND THE UNIVERSITY OF TEXAS AT AUSTIN HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
 import numpy
 import cv2
 from scipy import signal
--- main.py
+++ main.py
@@ -2,37 +2,47 @@ import subprocess
 import cv2
 from BRISQUE import brisque_score
 
-capture = cv2.VideoCapture('./resources/SampleVideo_1280x720_50mb.mp4')
-scale = ["wine svm-scale.exe -r allrange test_data >> test_data_scaled"]
-predict = ["wine svm-predict.exe -b 1 test_data_scaled allmodel output >> dump"]
-
+capture = cv2.VideoCapture('udp://@233.12.166.100:1234')
 next_frame = True
 while True:
     if next_frame:
         ret, frame = capture.read()
-        cv2.imshow('frame', frame)
-        next_frame = False
-        score = brisque_score(frame)
 
-        test_data = open("test_data", "w")
-        open("test_data_scaled", "w").close()
-        open("output", "w").close()
-        open("dump", "w").close()
-        index = 1
-        test_data.write(str(index) + " ")
+        if not ret:
+            capture.release()
+            capture = cv2.VideoCapture('udp://@233.12.166.100:1234')
+        else:
+            cv2.imshow('frame', frame)
+            #next_frame = False
+            score = brisque_score(frame)
 
-        for feat in score:
-            test_data.write(str(index) + ":" + str(feat) + " ")
-            index += 1
-        test_data.close()
+            test_data = open("test_data", "w")
+            open("test_data_scaled", "w").close()
+            open("output", "w").close()
+            open("dump", "w").close()
 
-        p = subprocess.Popen(scale, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        p = subprocess.Popen(predict, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        p.wait()
-        output = open("output", "r")
-        print(output.read())
-        output.close()
+            index = 1
+            test_data.write(str(index) + " ")
 
+            for feat in score:
+                test_data.write(str(index) + ":" + str(feat) + " ")
+                index += 1
+            test_data.close()
+
+            scale = ["wine svm-scale.exe -r allrange test_data >> test_data_scaled"]
+            predict = ["wine svm-predict.exe -b 1 test_data_scaled allmodel output >> dump"]
+            p1 = subprocess.Popen(scale, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            p1.wait()
+            p2 = subprocess.Popen(predict, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            p2.wait()
+            output = open("output", "r")
+            skit = open("score", "a")
+            skit.write('score: ' + output.read() + '\n')
+            skit.close()
+            output.close()
+
+            capture.release()
+            capture = cv2.VideoCapture('udp://@233.12.166.100:1234')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         next_frame = True
 
